a479044146bca3b7d62e657831ee9a511c248f8c
xianghang zeng <62739219+zengxianghang@users.noreply.github.com>
2026-08-26 21:54:34 +0800

    refactor: centralize atmosphere mode type

---
+++ include/gnss_sim/atmosphere_types.h
@@ -0,0 +1,14 @@
+#ifndef GNSS_SIM_ATMOSPHERE_TYPES_H_
+#define GNSS_SIM_ATMOSPHERE_TYPES_H_
+
+namespace gnss_sim {
+
+enum class AtmosphereMode {
+    UNSPECIFIED,
+    NONE,
+    BROADCAST,
+};
+
+} // namespace gnss_sim
+
+#endif // GNSS_SIM_ATMOSPHERE_TYPES_H_

998fe493b88c50ed7f52971cf20bcc379d039aad
xianghang zeng <62739219+zengxianghang@users.noreply.github.com>
2026-08-27 08:25:55 +0800

    feat: declare OEM7 solution writers

---
+++ src/output/novatel_solution_writer.h
@@ -0,0 +1,16 @@
+#ifndef GNSS_SIM_SRC_OUTPUT_NOVATEL_SOLUTION_WRITER_H_
+#define GNSS_SIM_SRC_OUTPUT_NOVATEL_SOLUTION_WRITER_H_
+
+#include "solution/solution_engine.h"
+
+#include <string>
+
+namespace gnss_sim {
+
+bool format_novatel_psrposa(const SolutionEpoch& solution, int tracked_satellites, std::string* message,
+                            std::string* error_message);
+bool format_novatel_psrvela(const SolutionEpoch& solution, std::string* message, std::string* error_message);
+
+} // namespace gnss_sim
+
+#endif // GNSS_SIM_SRC_OUTPUT_NOVATEL_SOLUTION_WRITER_H_

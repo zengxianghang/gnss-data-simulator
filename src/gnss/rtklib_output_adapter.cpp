7b4f4cd3afcf590538b0ed7a18aa27f08b3ecc15
xianghang zeng <62739219+zengxianghang@users.noreply.github.com>
2026-08-27 08:23:43 +0800

    feat: add RTKLIB satellite-id output adapter

---
+++ src/gnss/rtklib_output_adapter.cpp
@@ -0,0 +1,28 @@
+#include "gnss/rtklib_adapter.h"
+
+extern "C" {
+#include <rtklib.h>
+}
+
+#include <cstring>
+
+namespace gnss_sim {
+
+bool rtklib_satellite_number_to_id(int satellite_number, char satellite_id[4]) {
+    if (satellite_id == nullptr || satellite_number <= 0 || satellite_number > MAXSAT) {
+        return false;
+    }
+
+    char rtklib_id[16]{};
+    satno2id(satellite_number, rtklib_id);
+    if (std::strlen(rtklib_id) != 3) {
+        return false;
+    }
+    satellite_id[0] = rtklib_id[0];
+    satellite_id[1] = rtklib_id[1];
+    satellite_id[2] = rtklib_id[2];
+    satellite_id[3] = '\0';
+    return true;
+}
+
+} // namespace gnss_sim
